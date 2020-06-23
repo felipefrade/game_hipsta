8df02e70bc75e1757df3744ab802bb7532c4c53b
Felipe Frade <felipe.frade@outlook.com>
2020-06-23 12:32:16 -0300

    Primeiro commit - arquivos

---
+++ scripts/jogo/personagem.js
@@ -0,0 +1,13 @@
+class Personagem {
+  constructor(imagem) {   
+    this.imagem = imagem;
+  }
+  
+  exibe () {
+    image(this.imagem, 0, height - 135, 110, 135, 0, 0, 220, 270);
+  }
+  
+  anima() {
+    
+  }
+}
---
+++ sketch.js
@@ -0,0 +1,29 @@
+let imagemCenario; // armazenando as imagens na memória
+let imagemPersonagem;
+let cenario;
+let somDoJogo;
+let personagem;
+
+function preload() {
+  imagemCenario = loadImage ('imagens/cenario/floresta.png');
+  imagemPersonagem = loadImage ('imagens/personagem/correndo.png');
+  somDoJogo = loadSound('sons/trilha_jogo.mp3');
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  cenario = new Cenario(imagemCenario, 3);
+  personagem = new Personagem(imagemPersonagem);
+  somDoJogo.loop();
+}
+
+function draw() {
+  cenario.exibe();
+  cenario.move();
+  personagem.exibe();
+}
+
+function touchStarted() {
+  getAudioContext().resume()
+}
+
